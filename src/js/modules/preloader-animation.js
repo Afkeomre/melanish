dd88cc88292c1ac7d909f6289dd4c8515891ad71
Afkeomre <voronk0va.ir@yandex.ru>
2024-08-27 16:35:05 +0300

    test preloader

--- src/js/modules/preloader-animation.js
+++ src/js/modules/preloader-animation.js
@@ -1,27 +1,20 @@
 import { preloaderTime } from '../settings/constants';
 
 const preloaderAnimation = () => {
-  window.addEventListener('load', () => {
-    const preloader = document.querySelector('.preloader');
+  const preloader = document.querySelector('.preloader');
 
-    if (preloader.classList.contains('none')) {
-      return;
-    }
+  preloader.classList.add('preloader-animation');
+  document.body.classList.add('no-scroll');
 
-    preloader.classList.add('preloader-animation');
-    document.body.classList.add('no-scroll');
+  setTimeout(() => {
+    preloader.classList.remove('preloader-animation');
+    preloader.classList.add('preloader-hidden');
+  }, 6000);
 
-    setTimeout(() => {
-      preloader.classList.remove('preloader-animation');
-      preloader.classList.add('preloader-hidden');
-    }, 6000);
-
-    setTimeout(() => {
-      //startAnimation();
-      preloader.classList.add('preloader-none');
-      document.body.classList.remove('no-scroll');
-    }, preloaderTime);
-  });
+  setTimeout(() => {
+    preloader.classList.add('preloader-none');
+    document.body.classList.remove('no-scroll');
+  }, preloaderTime);
 };
 
 export default preloaderAnimation;
